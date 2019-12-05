88e6bedeffc49f3badae5a46723fddee082baace
Tim Knoll <timothy.knoll@gmail.com>
2019-12-05 02:45:44 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,19 +1,11 @@
 
-# def my_each (array)# put argument(s) here
-#   # code here
-#   i = 0
-#   while i < array.length
-#     yield (array[i])
-#     i += 1
-#   end
-#   array
-# end
-
-def my_each (array)
-  i = 0 
+def my_each (array)# put argument(s) here
+  # code here
+  i = 0
   while i < array.length
-    yield (array [i])
+    yield (array[i])
     i += 1
   end
   array
-end   
+end
+
